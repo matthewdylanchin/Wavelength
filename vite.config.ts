36fa9386da48e964ef0b8b37e4d79846547eed06
Matthew Dylan Chin <matthewdylanchin@gmail.com>
2026-04-18 11:50:23 +0100

    name update to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Wavelength/',
+  base: '/wavelength/',
   plugins: [
     react(),
     tailwindcss(),
